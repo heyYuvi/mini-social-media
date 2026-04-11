2355cff4a23c14e66a86670b1d1cae4837ed8492
heyYuvi <prasadyuvraj39@gmail.com>
2026-04-11 19:59:56 +0530

    Updated Post

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { api } from "../services/api";
 import { useNavigate } from "react-router-dom";
 import { IoSend } from "react-icons/io5";
+import { MdOutlineAddAPhoto } from "react-icons/md";
 
 
 const CreatePost = () => {
 
     const navigate = useNavigate();
+    const fileRef = useRef();
 
     const [content, setContent] = useState("");
     const [image, setImage] = useState(null);
@@ -14,14 +16,15 @@ const CreatePost = () => {
 
     const handlePost = async () => {
         if (!content.trim() && !image) {
-            return console.log("Post cannot be empty");
+            return alert("Post cannot be empty");
         }
         try {
-
             setLoading(true);
             const formData = new FormData();
             formData.append("content", content);
-            formData.append("image", image);
+            if(image){
+                 formData.append("image", image);
+            }
 
             const response = await api.post("/posts", formData, {
                 headers: {
@@ -31,12 +34,18 @@ const CreatePost = () => {
 
             setContent("");
             setImage(null);
-            setLoading(false);
             navigate("/feed");
+            fileRef.current.value = null;
         } catch (error) {
             console.error(error.response?.data?.message || "Create post error");
+        }finally{
+            setLoading(false);
         }
     }
+
+        const handleClick = () =>{
+            fileRef.current.click();
+        }
 
     return (
         <div className="mt-10 min-h-screen">
@@ -46,10 +55,16 @@ const CreatePost = () => {
                 <p className="text-gray-500 font-semi border-l border-blue-400 pl-2">Share your vision with gallery. Minimalist aesthics, maximum imapct.</p>
             </div>
             <div className="p-4 flex flex-col">
-                <textarea type="text" value={content} placeholder="What's on your mind?" onChange={(e) => { setContent(e.target.value) }} className="w-full  h-40 p-2 text-xl font-semibold bg-green-200 focus:outline-none mb-6"></textarea>
-                <input type="file" onChange={(e) => { setImage(e.target.files[0])}} className="w-full h-50 bg-blue-200 mb-4"></input>
+                <textarea type="text" value={content} placeholder="What's on your mind?" onChange={(e) => {setContent(e.target.value) }} className="w-full  h-40 p-2 text-xl font-semibold bg-green-200 focus:outline-none mb-6"></textarea>
+                <input type="file" ref={fileRef} onChange={(e) => { setImage(e.target.files[0])}} className="w-full h-50 bg-blue-200 mb-4 hidden"></input>
+                <div onClick={handleClick} className="w-full h-50 bg-blue-200 mb-4 flex flex-col items-center justify-center cursor-pointer">
+                    <MdOutlineAddAPhoto  size="35" className="text-blue-800 mb-3"/>
+                    <p className="text-gray-400 font-semibold">Upload low-res imagery</p>
+                    {image && (
+                        <img src={URL.createObjectURL(image)} alt="preview" className="w-full h-full object-cover"></img>
+                    )}
+                </div>
                 <button onClick={handlePost} disabled={loading} className="self-end bg-blue-400 p-2 font-bold text-white rounded-md hover:bg-blue-500 cursor-pointer">{loading ? "Posting" : (<div className="flex items-center gap-2">Post To Gallery<IoSend /></div>)}</button>
-
             </div>
          </div>
         </div>
